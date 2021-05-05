b4f0f082372dd9a048a271035273127a8fbf798c
Sheyin <Sheyin@users.noreply.github.com>
2021-05-05 14:34:04 -0500

    Finally got mouseover events sorted out on cards

--- index2script.js
+++ index2script.js
@@ -1,13 +1,29 @@
-// Adding event isteners
-/*
-const card_elements = document.getElementsByClassName('card');
-for(let i=0; i<card_elements.length, i++;) {
-	card.addEventListener('click', doFlip);
-} */
 
-const doFlip = (id_name) => {
-	//onst whichCard = event.target.id;
-	const card = document.getElementById(id_name);
-	card.classList.toggle('front');
-	card.classList.toggle('back');
-};
+window.addEventListener('load', (event) => {
+	console.log('page is fully loaded');
+	// Adding event isteners to the cards, which makes text visible on mouseover
+	const cards = document.getElementsByClassName('card');
+/*
+	for(var i=0; i < 10; i++){
+		console.log(i);
+	} */
+
+	for(let i=0; i<cards.length; i++) {
+		//card.addEventListener('click', doFlip);
+		cards[i].addEventListener('mouseenter', doMakeTextVisible);
+		cards[i].addEventListener('mouseleave', doMakeTextVisible);
+	} 
+  });
+
+
+const doMakeTextVisible = (event) => {
+	console.log("moused over");
+	console.log(event.target.id);
+	const card = document.getElementById(event.target.id);
+	//card.classList.toggle('visible-card-content');
+	for(let i=0; i<card.children.length; i++) {
+		if (card.children[i].classList.contains('hidden-card-content')) {
+			card.children[i].classList.toggle('visible-card-content');
+		}
+	} 
+}
